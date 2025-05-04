5b6efb4c9d6b405b1fd572d7a39f46d5e6b77645
Jack Sipley <sipleyjack@gmail.com>
2025-05-04 10:34:54 -0400

    Initial Control & Note Interaction Logic

--- Assets/Scripts/ControlArrowBehavior.cs
+++ Assets/Scripts/ControlArrowBehavior.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class ControlArrowBehavior : MonoBehaviour
 {
+    private bool _isActive = false;
+
+    public bool IsActive
+    {
+        get { return _isActive; }
+        set
+        {
+            _isActive = value;
+            SetActive(_isActive);
+        }
+    }
+    
     public void SetActive(bool isActive)
     {
         if (isActive)
         {
             GetComponent<Renderer>().material.color = Color.yellow;
+            _isActive = true;
         }
         else
         {
--- Assets/Scripts/NoteBehavior.cs
+++ Assets/Scripts/NoteBehavior.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class NoteBehavior : MonoBehaviour
 {
-    public float speed = 2.5f; // Speed of note movement in positive Y direction
+    public float speed = 2.5f;
+    private bool _noteMissed = false;
     void Start()
     {
-
     }
 
     void Update()
@@ -17,5 +17,33 @@ public class NoteBehavior : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    } 
+    }
+
+    // If a control was pressed before a collison between the note and control was
+    // triggered, consider the event a miss. 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Control_Arrow"))
+        {
+            return;
+        }
+        if (collision.gameObject.GetComponent<ControlArrowBehavior>().IsActive)
+        {
+            Debug.Log("MISSED Note: Control held down prematurely");
+            _noteMissed = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Control_Arrow") || _noteMissed)
+        {
+            return;
+        }
+        if (collision.gameObject.GetComponent<ControlArrowBehavior>().IsActive)
+        {
+            Destroy(gameObject);
+            Debug.Log("HIT Note");
+        }
+    }
 }
